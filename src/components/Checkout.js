f04236b1ab6d20d8d3c675d7afdffc698b85b62e
Mrudula Didde <5180264+shinydidde@users.noreply.github.com>
2024-07-08 15:54:08 +0100

    added an Accordion in checkout page

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -6,6 +6,10 @@ import axios from 'axios';
 import { useTranslation } from 'react-i18next';
 import { CartContext } from '../context/CartContext';
 import { useAuth } from '../context/AuthContext';
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import GoogleLoginButton from './GoogleLoginButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { MuiTelInput, matchIsValidTel } from 'mui-tel-input';
@@ -42,7 +46,7 @@ const useStyles = makeStyles((theme) => ({
         margin: theme.spacing(0, 1),
     },
     img: {
-        width: '250px'
+        width: '100px'
     },
     dateInput: {
         marginBottom: theme.spacing(2),
@@ -70,6 +74,11 @@ const Checkout = () => {
     const [phoneNumber, setPhoneNumber] = useState('');
     const [phoneNumberError, setPhoneNumberError] = useState('');
     const [open, setOpen] = useState(false); // State to control dialog
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleChange = (panel) => (event, isExpanded) => {
+        setExpanded(isExpanded ? panel : false);
+    };
 
     const handleRemove = (id) => {
         removeFromCart(id);
@@ -112,36 +121,36 @@ const Checkout = () => {
 
     const handleCheckout = async () => {
         if (!currentUser) {
-          alert(t('loginToBook'));
-          return;
+            alert(t('loginToBook'));
+            return;
         }
         if (!matchIsValidTel(phoneNumber)) {
-          setPhoneNumberError(t('Invalid phone number'));
-          return;
+            setPhoneNumberError(t('Invalid phone number'));
+            return;
         }
 
         try {
-          const response = await axios.post('https://u13u7uffbc.execute-api.eu-west-1.amazonaws.com/Development/booking', {
-            userId: currentUser.uid,
-            services: cart.map((service) => ({
-              id: service.id,
-              title: service.title,
-              price: service.price,
-              count: itemCounts[service.id],
-            })),
-            startDate: startDate.toISOString(),
-            endDate: endDate ? endDate.toISOString() : null,
-            phoneNumber: phoneNumber,
-          });
+            const response = await axios.post('https://u13u7uffbc.execute-api.eu-west-1.amazonaws.com/Development/booking', {
+                userId: currentUser.uid,
+                services: cart.map((service) => ({
+                    id: service.id,
+                    title: service.title,
+                    price: service.price,
+                    count: itemCounts[service.id],
+                })),
+                startDate: startDate.toISOString(),
+                endDate: endDate ? endDate.toISOString() : null,
+                phoneNumber: phoneNumber,
+            });
 
-          const { bookingId } = response.data; // Assuming response has bookingId
+            const { bookingId } = response.data; // Assuming response has bookingId
 
-          clearCart();
-          navigate(`/confirmation?bookingId=${bookingId}`);
+            clearCart();
+            navigate(`/confirmation?bookingId=${bookingId}`);
         } catch (error) {
-          console.error('Error during booking:', error);
+            console.error('Error during booking:', error);
         }
-      };
+    };
 
     const handleClickOpen = () => {
         setOpen(true);
@@ -173,94 +182,128 @@ const Checkout = () => {
                 </Paper>
             ) : (
                 <div>
-                    {cart.map((service) => (
-                        <Paper className={classes.paper} key={service.id}>
-                            <Box className={classes.item}>
-                                <img className={classes.img} src={service.image} alt='' />
-                                <Typography variant="h6">{service.title} - €{service.price}</Typography>
-                                <Box className={classes.itemCount}>
-                                    <IconButton
-                                        color='secondary'
-                                        className={classes.itemCountButton}
-                                        onClick={() => handleRemove(service.id)}
-                                    ><DeleteIcon /></IconButton>
+                    <Accordion defaultExpanded expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1bh-content"
+                            id="panel1bh-header"
+                        >
+                            <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                                Cart Items
+                            </Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            {cart.map((service) => (
+                                <Paper className={classes.paper} key={service.id}>
+                                    <Box className={classes.item}>
+                                        <img className={classes.img} src={service.image} alt='' />
+                                        <Typography variant="h6">{service.title} - €{service.price}</Typography>
+                                        <Box className={classes.itemCount}>
+                                            <IconButton
+                                                color='secondary'
+                                                className={classes.itemCountButton}
+                                                onClick={() => handleRemove(service.id)}
+                                            ><DeleteIcon /></IconButton>
+                                        </Box>
+                                    </Box>
+                                </Paper>
+                            ))}
+                        </AccordionDetails>
+                    </Accordion>
+                    <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel2bh-content"
+                            id="panel2bh-header"
+                        >
+                            <Typography sx={{ width: '33%', flexShrink: 0 }}>{t('Booking Details')}</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Paper className={classes.paper}>
+                                <Box>
+                                    <TextField
+                                        id="start-date"
+                                        label={t('Start Date')}
+                                        type="date"
+                                        value={startDate.toISOString().slice(0, 10)}
+                                        onChange={handleStartDateChange}
+                                        className={classes.dateInput}
+                                        InputLabelProps={{
+                                            shrink: true,
+                                        }}
+                                        required
+                                        inputProps={{
+                                            min: new Date().toISOString().slice(0, 10),
+                                        }}
+                                    />
+                                    <TextField
+                                        id="end-date"
+                                        label={t('End Date')}
+                                        type="date"
+                                        value={endDate ? endDate.toISOString().slice(0, 10) : ''}
+                                        onChange={handleEndDateChange}
+                                        className={classes.dateInput}
+                                        InputLabelProps={{
+                                            shrink: true,
+                                        }}
+                                        inputProps={{
+                                            min: minEndDate.toISOString().slice(0, 10), // Minimum end date is the day after start date
+                                        }}
+                                    />
+                                    <MuiTelInput
+                                        id="phone-number"
+                                        label={t('Mobile')}
+                                        value={phoneNumber}
+                                        defaultCountry="IE"
+                                        onChange={handlePhoneNumberChange}
+                                        className={classes.dateInput}
+                                        required
+                                        error={Boolean(phoneNumberError)}
+                                        helperText={phoneNumberError}
+                                    />
                                 </Box>
-                            </Box>
-                        </Paper>
-                    ))}
-                    <Paper className={classes.paper}>
-                        <Typography variant="h6">{t('Booking Details')}</Typography>
-                        <br />
-                        <Box>
-                            <TextField
-                                id="start-date"
-                                label={t('Start Date')}
-                                type="date"
-                                value={startDate.toISOString().slice(0, 10)}
-                                onChange={handleStartDateChange}
-                                className={classes.dateInput}
-                                InputLabelProps={{
-                                    shrink: true,
-                                }}
-                                required
-                                inputProps={{
-                                    min: new Date().toISOString().slice(0, 10),
-                                }}
-                            />
-                            <TextField
-                                id="end-date"
-                                label={t('End Date')}
-                                type="date"
-                                value={endDate ? endDate.toISOString().slice(0, 10) : ''}
-                                onChange={handleEndDateChange}
-                                className={classes.dateInput}
-                                InputLabelProps={{
-                                    shrink: true,
-                                }}
-                                inputProps={{
-                                    min: minEndDate.toISOString().slice(0, 10), // Minimum end date is the day after start date
-                                }}
-                            />
-                            <MuiTelInput
-                                id="phone-number"
-                                label={t('Mobile')}
-                                value={phoneNumber}
-                                defaultCountry="IE"
-                                onChange={handlePhoneNumberChange}
-                                className={classes.dateInput}
-                                required
-                                error={Boolean(phoneNumberError)}
-                                helperText={phoneNumberError}
-                            />
-                        </Box>
-                    </Paper>
-                    <Paper className={classes.summary}>
-                        <Typography variant="h6">{t('paymentSummary')}</Typography>
-                        <Box className={classes.item}>
-                            <Typography variant="body1">{t('itemTotal')}</Typography>
-                            <Typography variant="body1">€{calculateTotal()}</Typography>
-                        </Box>
-                        <Box className={classes.item}>
-                            <Typography variant="body1">{t('taxesAndFee')}</Typography>
-                            <Typography variant="body1">€49</Typography>
-                        </Box>
-                        <Divider className={classes.section} />
-                        <Box className={classes.item}>
-                            <Typography variant="body1">{t('total')}</Typography>
-                            <Typography variant="body1">€{totalAmount}</Typography>
-                        </Box>
-                        <Box className={classes.item}>
-                            <Typography variant="body1" fontWeight="bold">
-                                {t('amountToPay')}
+                            </Paper>
+                        </AccordionDetails>
+                    </Accordion>
+                    <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel3bh-content"
+                            id="panel3bh-header"
+                        >
+                            <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                                {t('paymentSummary')}
                             </Typography>
-                            <Typography variant="body1" fontWeight="bold">
-                                €{totalAmount}
-                            </Typography>
-                        </Box><br />
-                        <Button variant="contained" color="secondary" onClick={handleClickOpen} disabled={!matchIsValidTel(phoneNumber)}>
-                            {t('checkout')}
-                        </Button>
-                    </Paper>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Paper className={classes.summary}>
+                                <Box className={classes.item}>
+                                    <Typography variant="body1">{t('itemTotal')}</Typography>
+                                    <Typography variant="body1">€{calculateTotal()}</Typography>
+                                </Box>
+                                <Box className={classes.item}>
+                                    <Typography variant="body1">{t('taxesAndFee')}</Typography>
+                                    <Typography variant="body1">€49</Typography>
+                                </Box>
+                                <Divider className={classes.section} />
+                                <Box className={classes.item}>
+                                    <Typography variant="body1">{t('total')}</Typography>
+                                    <Typography variant="body1">€{totalAmount}</Typography>
+                                </Box>
+                                <Box className={classes.item}>
+                                    <Typography variant="body1" fontWeight="bold">
+                                        {t('amountToPay')}
+                                    </Typography>
+                                    <Typography variant="body1" fontWeight="bold">
+                                        €{totalAmount}
+                                    </Typography>
+                                </Box><br />
+                                <Button variant="contained" color="secondary" onClick={handleClickOpen} disabled={!matchIsValidTel(phoneNumber)}>
+                                    {t('checkout')}
+                                </Button>
+                            </Paper>
+                        </AccordionDetails>
+                    </Accordion>
                 </div>
             )}
 
@@ -269,11 +312,11 @@ const Checkout = () => {
                 <DialogContent>
                     <DialogContentText>
                         {t('Please review your booking and cart details before confirming.')}
-                    </DialogContentText><br/>
+                    </DialogContentText><br />
                     <Typography variant="h6">{t('Booking Details')}</Typography>
                     <Typography>{t('Start Date')}: {startDate.toISOString().slice(0, 10)}</Typography>
                     <Typography>{t('End Date')}: {endDate ? endDate.toISOString().slice(0, 10) : t('N/A')}</Typography>
-                    <Typography>{t('Mobile')}: {phoneNumber}</Typography><br/>
+                    <Typography>{t('Mobile')}: {phoneNumber}</Typography><br />
                     <Typography variant="h6" className={classes.section}>{t('Cart Details')}</Typography>
                     {cart.map((service) => (
                         <Box key={service.id}>
