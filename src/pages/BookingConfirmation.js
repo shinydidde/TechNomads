8e980c92add1b0eef823f29c9574ade1566e0239
Mrudula Didde <5180264+shinydidde@users.noreply.github.com>
2024-07-08 23:50:43 +0100

    Booking Confirmation and translations

--- src/pages/BookingConfirmation.js
+++ src/pages/BookingConfirmation.js
@@ -4,14 +4,16 @@ import { makeStyles } from '@mui/styles';
 import { useTranslation } from 'react-i18next';
 import axios from 'axios';
 import { useLocation } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext'; // Assuming you have an AuthContext to provide current user info
+import GoogleLoginButton from '../components/GoogleLoginButton';
 
 const useStyles = makeStyles((theme) => ({
     root: {
         marginTop: theme.spacing(4),
     },
     paper: {
-        padding: theme.spacing(4),
-        marginBottom: theme.spacing(4),
+        padding: theme.spacing(2),
+        marginBottom: theme.spacing(2),
     },
     bookingDetails: {
         marginBottom: theme.spacing(2),
@@ -23,56 +25,80 @@ const BookingConfirmation = () => {
     const { t } = useTranslation();
     const location = useLocation();
     const bookingId = new URLSearchParams(location.search).get('bookingId'); // Extract bookingId from query param
+    const { currentUser } = useAuth(); // Get the current user from AuthContext
 
     const [bookingDetails, setBookingDetails] = useState(null);
 
     useEffect(() => {
-        if (bookingId) {
+        if (bookingId && currentUser) { // Only fetch booking details if the user is logged in
             // Make API call to fetch booking details
             axios.get(`https://u13u7uffbc.execute-api.eu-west-1.amazonaws.com/Development/confirmation/${bookingId}`)
                 .then(response => {
-                    setBookingDetails(response.data);
+                    setBookingDetails(response.data.body);
                 })
                 .catch(error => {
                     console.error('Error fetching booking details:', error);
                 });
         }
-    }, [bookingId]);
+    }, [bookingId, currentUser]);
 
-    if (!bookingDetails) {
-        return null; // Optionally, show loading indicator or error message if data is being fetched
+    if (!currentUser) {
+        return (
+            <Container className={classes.root}>
+                <Typography variant="h4" gutterBottom>
+                    {t('bookingConfirmation')}
+                </Typography>
+                <Paper className={classes.paper}>
+                    <Typography variant="body1">{t('loginToViewBooking')}</Typography><br />
+                    <GoogleLoginButton />
+                </Paper>
+            </Container>
+        );
     }
 
-    console.log(bookingDetails);
+    if (!bookingDetails) {
+        return (
+            <Container className={classes.root}>
+                <Typography variant="h4" gutterBottom>
+                    {t('bookingConfirmation')}
+                </Typography>
+                <Paper className={classes.paper}>
+                    <Typography variant="body1">
+                        {t('loadingBookingDetails')}
+                    </Typography>
+                </Paper>
+            </Container>
+        );
+    }
 
     return (
         <Container className={classes.root}>
             <Typography variant="h4" gutterBottom>
-                {t('Booking Confirmation')}
+                {t('bookingConfirmation')}
             </Typography>
             <Paper className={classes.paper}>
                 <Typography variant="h6" className={classes.bookingDetails}>
-                    {t('Booking ID')}: {bookingDetails.bookingId}
+                    {t('bookingId')}: {bookingDetails.bookingId}
                 </Typography>
                 <Typography variant="body1" className={classes.bookingDetails}>
-                    {t('Start Date')}: {bookingDetails.startDate}
+                    {t('startDate')}: {bookingDetails.startDate}
                 </Typography>
                 <Typography variant="body1" className={classes.bookingDetails}>
-                    {t('End Date')}: {bookingDetails.endDate}
+                    {t('endDate')}: {bookingDetails.endDate}
                 </Typography>
                 <Typography variant="body1" className={classes.bookingDetails}>
-                    {t('Mobile')}: {bookingDetails.phoneNumber}
+                    {t('mobile')}: {bookingDetails.phoneNumber}
                 </Typography>
-                <Typography variant="h6" className={classes.bookingDetails}>
-                    {t('Services Booked')}
+                <Typography variant="h6" mt={2} className={classes.bookingDetails}>
+                    {t('servicesBooked')}
                 </Typography>
                 {bookingDetails.services.map((service) => (
                     <Typography key={service.id} variant="body1" className={classes.bookingDetails}>
-                        {service.title} - €{service.price}
+                        {t(service.title)} - €{service.price}
                     </Typography>
                 ))}
-                <Typography variant="h6" className={classes.bookingDetails}>
-                    {t('Total Amount')}: €{bookingDetails.totalAmount}
+                <Typography variant="h6" mt={2} className={classes.bookingDetails}>
+                    {t('totalAmount')}: €{bookingDetails.totalAmount}
                 </Typography>
             </Paper>
         </Container>
